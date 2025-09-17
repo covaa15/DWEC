a16ec84b1c886beb4e7bac97882427d754f60986
covaa15 <covadongabustantigo@gmail.com>
2025-09-17 11:19:08 +0200

    Fin del ejercicio 3

--- Ejercicio1/scripts/ej_03.js
+++ Ejercicio1/scripts/ej_03.js
@@ -20,4 +20,27 @@ let pedido={
 //4.Mostrar el objeto Pedido
 console.table(pedido)
 
-//5.
+//5.Crear un nuevo objeto con la propiedad nombre y combinarlo con cliente y ver que sucede
+
+let producto2 = {
+    nombre:"Reloj"
+}
+let cliente2={
+    nombre:"Eneas",
+    esPremium:false
+}
+let combinarlo ={
+    ...cliente2,
+    ...producto2
+}
+
+console.table(combinarlo)
+
+/* Al combinar dos objetos que tienen una propiedad con el mismo nombre,
+ * la propiedad del primer objeto se sobrescribe con la del segundo.
+ * En este caso, como primero se pasa cliente2 y después producto2 y ambos
+ * contienen la propiedad nombre.
+ * La propiedad "nombre" de cliente2 es machacada por la de producto2 y
+ * finalmente, se mostraría por pantalla: el nombre del producto,
+ * y si el cliente es premium o no
+ */
