d0b23e148901d6fca54004624e5be2401bce3874
covaa15 <covadongabustantigo@gmail.com>
2025-10-20 22:02:47 +0200

    Ejercicio7_1

---
+++ Ejer_07/scripts/main.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('%cDocumento listo.', 'color: green; font-size: 16px; font-weight: bold;');
+    console.log('%cEscribe las soluciones en main.js', 'color: red; font-size: 18px; font-weight: bold;');
+
+
+    // --- Solución Ejercicio 1 y 4 ---
+
+    const contenedorPadre = document.getElementById('outer-box');
+
+    contenedorPadre.addEventListener('click', function (event) {
+        if (event.target !== contenedorPadre) {
+            quitarColor();
+            if (event.target) {
+                //Cambio el color al div hijo clicado
+                document.getElementById(event.target.id).classList.add('color');
+
+                //Muestro las ID por consola
+                console.log('ID Elemento pulsado:', event.target.id)
+                console.log('ID Elemento Padre:', event.currentTarget.id)
+            }
+
+        }
+
+    });
+
+    //Funcion que quita el color al hacer click en otro hijo
+    function quitarColor() {
+        contenedorPadre.querySelectorAll('div').forEach((contenedor) => {
+            contenedor.classList.remove('color');
+        })
+    }
+
+
+
+
+  
+
+
+
+    // --- Solución Ejercicio 2 ---
+
+
+    // --- Solución Ejercicio 3 ---
+
+
+    // --- Solución Ejercicio 5 ---
+
+});
