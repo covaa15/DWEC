7e7c911c1404a636f9343db3f14dfb6245577e4f
covaa15 <covadongabustantigo@gmail.com>
2026-01-12 23:20:03 +0100

    Añadi el ejerrcicio 1 a una carpeta

---
+++ Ejer_12/Ejercicio1/scripts/ejercicio1.js
@@ -0,0 +1,121 @@
+let productos = [];
+
+cargarProductos("../data/productos.json");
+const categorias = [];
+const selector = document.querySelector("#selector");
+const contenedorPadre = document.querySelector("#padre");
+const menorMayor = document.querySelector('#menorMayor');
+const mayorMenor = document.querySelector('#mayorMenor');
+
+
+//Función para cargar los productos
+async function cargarProductos(ruta) {
+
+    const cargando = document.createElement("p");
+    cargando.innerText = "Cargando...";
+    document.body.appendChild(cargando);
+
+    await fetch(ruta)
+        .then((respuesta) => {
+            return respuesta.json();
+        }).then((datos) => {
+            productos = datos;
+            cargando.remove();
+            verProductos(productos);
+            cargarCategorias();
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+
+}
+
+//Funcion para mostrar los productos 
+function verProductos(productos) {
+
+    productos.map((producto) => {
+        let div = crearDiv('hijo');
+        crearH3(div, producto.nombre);
+        crearP(div, "Precio: " + producto.precio);
+        crearP(div, "Stock: " + producto.stock);
+        crearP(div, "Categoría: " + producto.categoria);
+        contenedorPadre.appendChild(div);
+    });
+}
+
+//Función para cargar las categorias
+function cargarCategorias() {
+    productos.map((producto) => {
+
+        if (!categorias.includes(producto.categoria)) {
+            categorias.push(producto.categoria);
+            crearOpciones(selector, producto.categoria);
+        }
+
+    });
+}
+
+//Función que crea los div
+function crearDiv(id) {
+    const div = document.createElement('div');
+    div.id = id;
+    return div;
+}
+
+//Función que crea los p
+function crearP(div, texto) {
+    const p = document.createElement('p');
+    p.textContent = texto;
+    div.appendChild(p);
+}
+
+//Funcion que crea los h3
+function crearH3(div, texto) {
+    const h3 = document.createElement('h3');
+    h3.textContent = texto;
+    div.appendChild(h3);
+}
+
+//Funcion para crear las opciones del select
+function crearOpciones(selector, texto) {
+    const opcion = document.createElement('option');
+    opcion.textContent = texto;
+    selector.appendChild(opcion);
+
+}
+
+
+//Aplico los filtros
+function aplicarFiltros(accion) {
+    contenedorPadre.innerHTML = "";
+    let productosFiltrados = productos;
+    const categoriaSeleccionada = selector.value.toLocaleLowerCase();
+
+    if (categoriaSeleccionada !== '' && categoriaSeleccionada !== 'todas') {
+        productosFiltrados = productosFiltrados.filter((producto) => {
+            if (producto.categoria.toLocaleLowerCase() === categoriaSeleccionada)
+                return producto;
+
+        });
+    }
+
+    if (accion === "mayorMenor") {
+        productosFiltrados = productosFiltrados.sort((a, b) => b.precio - a.precio);
+
+    } else {
+        productosFiltrados = productosFiltrados.sort((a, b) => a.precio - b.precio);
+    }
+
+
+    verProductos(productosFiltrados);
+}
+selector.addEventListener("change", function () {
+    aplicarFiltros("");
+});
+
+menorMayor.addEventListener("click", function () {
+    aplicarFiltros("menorMayor")
+});
+mayorMenor.addEventListener("click", function () {
+    aplicarFiltros("mayorMenor")
+});
